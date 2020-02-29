ff7f9c24205e4adda0ae0ea85be929e262dd4509
MariaPoliti <politim@uw.edu>
2020-02-28 16:14:48 -0800

    Implemented automatic file name change for multiple file creation. Added folder for simulated data.

--- data/simulation/data_simulation.py
+++ data/simulation/data_simulation.py
@@ -1,18 +1,19 @@
 import csv
-import numpy as np
 import os
-import pandas as pd
-import PyEIS
 import time
+
+import numpy as np
+import pandas as pd
+
 from PyEIS import *
 
 
 def to_dataframe(f_range, real_z, imag_z, total_z, phase):
     """
     """
-    sim_data = {'freq[Hz]': f_range[0], 'angular_freq[1/s]': f_range[1],
+    sim_data = {'freq [Hz]': f_range[0], 'angular_freq [1/s]': f_range[1],
                 'Re_Z [Ohm]': real_z, 'Im_Z [Ohm]': imag_z, '|Z| [Ohm]':
-                total_z, 'phase_angle': phase}
+                total_z, 'phase_angle [rad]': phase}
     sim_data_df = pd.DataFrame(sim_data)
     return sim_data_df
 
@@ -35,26 +36,34 @@ def rc_simulation(f_start, f_stop, decades, R, C, i):
 
 
 def sim_rc_file_writer(f_start, f_stop, decades, R, C, i,
-                       alteration='none'):
+                       alteration=None):
     """
     """
     # Save the simulated data in a csv file to be exported in a database
     # the format will be in the form of :
     # yymmdd-serial#_sim_classifier.csv
     date = time.strftime('%y%m%d', time.localtime())
-    number = str(i).zfill(4)
-    if alteration != 'none':
-        filename = str('{}-{}_sim_one-{}.csv'.format(date, number, alteration))
-        # while os.path.exists('')
-    else:
-        filename = str('{}-{}_sim_one.csv'.format(date, number))
-
-    with open(filename + ".csv", mode='a', newline='') as data_file:
-        data_file.write('Date: {}'.format(date)+'\n')
-        data_file.write('Serial number: {}'.format(number)+'\n')
-        data_file.write('Data Source: simulation'+'\n')
-        data_file.write('Circuit type: rc'+'\n')
-        data_file.write('Circuit elements:[R={} ohm,C={} F]'.format(R, C)+'\n')
+    save_location = 'sim_data/'
+    if not os.path.exists(save_location):
+        os.makedirs(save_location)
+    while i < 9999:
+        number = str(i).zfill(4)
+        if alteration:
+            filename = str('{}-{}_sim_one-{}'.format(date, number, alteration))
+        else:
+            filename = str('{}-{}_sim_one'.format(date, number))
+        if os.path.exists(save_location + filename + '.csv'):
+            i += 1
+        else:
+            break
+    with open(save_location + filename + ".csv", mode='a',
+              newline='') as data_file:
+        data_file.write('Date:, {}'.format(date)+'\n')
+        data_file.write('Serial number:, {}'.format(number)+'\n')
+        data_file.write('Data Source:, simulation'+'\n')
+        data_file.write('Circuit type:, rc'+'\n')
+        data_file.write('Circuit elements:, [R={} ohm C={} F]'.format(R, C) +
+                        '\n')
         data_file.write('---'+'\n')
         df = rc_simulation(f_start, f_stop, decades, R, C, i)
         df.to_csv(data_file, mode='a')
