c05621da988cd115c944a7a355848eb237669558
amoeezuw <amoeez@uw.edu>
2020-03-01 13:45:19 -0800

    Function for randles added for simulation as well as for file writer

--- data/simulation/data_simulation.py
+++ data/simulation/data_simulation.py
@@ -68,3 +68,53 @@ def sim_rc_file_writer(f_start, f_stop, decades, R, C, i,
         df = rc_simulation(f_start, f_stop, decades, R, C, i)
         df.to_csv(data_file, mode='a')
         data_file.close()
+
+def randles(f_start, f_stop, decades, Rs, R, n, sigma, Q):
+    """
+    """
+    # Define the frequency range to be simulated
+    f_range = freq_gen(f_start, f_stop, decades)
+    # Obtain the impedance of the RC circuit
+    Randles = cir_Randles_simplified(f_range[1], Rs, R, n, sigma, Q)
+    # Separate the impedance into its real and imaginary components
+    real_z = Randles.real
+    imag_z = Randles.imag
+    # Calcuate the magnitude and phase angle of the impedance
+    phase = np.arctan(imag_z/real_z)
+    total_z = np.sqrt((real_z**2) + (imag_z**2))
+    sim_data = to_dataframe(f_range, real_z, imag_z, total_z, phase)
+    return sim_data
+
+def sim_randles_file_writer(f_start, f_stop, decades, Rs, R, n, sigma, Q, alteration=None):
+    """
+    """
+    # Save the simulated data in a csv file to be exported in a database
+    # the format will be in the form of :
+    # yymmdd-serial#_sim_classifier.csv
+    i=1
+    date = time.strftime('%y%m%d', time.localtime())
+    save_location = 'sim_data/'
+    if not os.path.exists(save_location):
+        os.makedirs(save_location)
+    while i < 9999:
+        number = str(i).zfill(4)
+        if alteration:
+            filename = str('{}-{}_sim_tail-{}'.format(date, number, alteration))
+        else:
+            filename = str('{}-{}_sim_tail'.format(date, number))
+        if os.path.exists(save_location + filename + '.csv'):
+            i += 1
+        else:
+            break
+    with open(save_location + filename + ".csv", mode='a',
+              newline='') as data_file:
+        data_file.write('Date:, {}'.format(date)+'\n')
+        data_file.write('Serial number:, {}'.format(number)+'\n')
+        data_file.write('Data Source:, simulation'+'\n')
+        data_file.write('Circuit type:, rc'+'\n')
+        data_file.write('Circuit elements:, [R={} ohm Rs ={} ohm sigma = {} ohm Q = {} F]'.format(R, Rs, sigma, Q) +
+                        '\n')
+        data_file.write('---'+'\n')
+        df = randles(f_start, f_stop, decades, Rs, R, n, sigma, Q)
+        df.to_csv(data_file, mode='a')
+        data_file.close()
