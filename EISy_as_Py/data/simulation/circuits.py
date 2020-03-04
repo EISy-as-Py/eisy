1be4deb829b6ab8d3ea803157b5b4c6e68f7ddaa
amoeezuw <amoeez@uw.edu>
2020-03-03 22:54:30 -0800

    The circuit model for RsRQRQ has been added. Testing needs to be done. The output should create two semi circles

--- EISy_as_Py/data/simulation/circuits.py
+++ EISy_as_Py/data/simulation/circuits.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def cir_RC(w, C='none', R='none', fs='none'):   
+def cir_RC(w, C='none', R='none', fs='none'):
     '''
     Simulation Function: -RC-
-    Returns the impedance of an RC circuit, using RQ definations where n=1. 
+    Returns the impedance of an RC circuit, using RQ definations where n=1.
     see cir_RQ() for details
     Kristian B. Knudsen (kknu@berkeley.edu || kristianbknudsen@gmail.com)
     Inputs
@@ -21,8 +21,9 @@ def cir_RC(w, C='none', R='none', fs='none'):
 def cir_RQ(w, R='none', Q='none', n='none', fs='none'):
     '''
     Simulation Function: -RQ-
-    Return the impedance of an Rs-RQ circuit. See details for RQ under cir_RQ_fit() 
-    Kristian B. Knudsen (kknu@berkeley.edu / kristianbknudsen@gmail.com)   
+    Return the impedance of an Rs-RQ circuit. See details for RQ under
+    cir_RQ_fit()
+    Kristian B. Knudsen (kknu@berkeley.edu / kristianbknudsen@gmail.com)
     Inputs
     ----------
     w = Angular frequency [1/s]
@@ -59,3 +60,43 @@ def cir_Randles_simplified(w, Rs, R, n, sigma, Q='none', fs='none'):
     Z_w = sigma*(w**(-0.5))-1j*sigma*(w**(-0.5))
 
     return Rs + 1/(1/Z_Q + 1/(Z_R+Z_w))
+
+
+def cir_RsRQRQ(w, Rs, R='none', Q='none', n='none', fs='none', R2='none',
+               Q2='none', n2='none', fs2='none'):
+    '''
+
+    Simulation Function: -Rs-RQ-RQ-
+    Return the impedance of an Rs-RQ circuit. See details for RQ under
+    cir_RQ_fit()
+    Kristian B. Knudsen (kknu@berkeley.edu || kristianbknudsen@gmail.com)
+
+    Inputs
+    ----------
+    w = Angular frequency [1/s]
+    Rs = Series Resistance [Ohm]
+    R = Resistance [Ohm]
+    Q = Constant phase element [s^n/ohm]
+    n = Constant phase element exponent [-]
+    fs = Summit frequency of RQ circuit [Hz]
+    R2 = Resistance [Ohm]
+    Q2 = Constant phase element [s^n/ohm]
+    n2 = Constant phase element exponent [-]
+    fs2 = Summit frequency of RQ circuit [Hz]
+    '''
+
+    if R == 'none':
+        R = (1/(Q*(2*np.pi*fs)**n))
+    elif Q == 'none':
+        Q = (1/(R*(2*np.pi*fs)**n))
+    elif n == 'none':
+        n = np.log(Q*R)/np.log(1/(2*np.pi*fs))
+
+    if R2 == 'none':
+        R2 = (1/(Q2*(2*np.pi*fs2)**n2))
+    elif Q2 == 'none':
+        Q2 = (1/(R2*(2*np.pi*fs2)**n2))
+    elif n2 == 'none':
+        n2 = np.log(Q2*R2)/np.log(1/(2*np.pi*fs2))
+
+    return Rs + (R/(1+R*Q*(w*1j)**n)) + (R2/(1+R2*Q2*(w*1j)**n2))
