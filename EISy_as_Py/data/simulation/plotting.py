2863a79224f80f88b42060237dc757edd4ae9e76
amoeezuw <amoeez@uw.edu>
2020-03-03 22:27:58 -0800

    Fixed the plotting function by lowering down the plt.show(). The image is margins are not correct

--- EISy_as_Py/data/simulation/plotting.py
+++ EISy_as_Py/data/simulation/plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-def plot_nyquist(response, save_image='None'):
+
+def plot_nyquist(response, save_image=None):
     """
     """
     fig = plt.figure()
@@ -8,7 +9,8 @@ def plot_nyquist(response, save_image='None'):
     plt.ticklabel_format(style='sci', scilimits=(0, 0))
     plt.xlabel('Z$_{real}$ [$\Omega$]')
     plt.ylabel('-Z$_{imag}$ [$\Omega$]')
-    plt.show()
 
     if save_image:
         plt.savefig('image_name.png')
+
+    plt.show()
