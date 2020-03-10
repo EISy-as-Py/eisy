5fec4f07f5a6471b771fe6211f0a8b5832caa6b1
David Hurt <fightingtesla@gmail.com>
2020-03-10 13:18:03 -0700

    trying to get tests to work

--- EISy_as_Py/__init__.py
+++ EISy_as_Py/__init__.py
@@ -1,4 +1,5 @@
 from .version import __version
 from EISy_as_Py import *
+from eisy_file_mgmt import *
 
 __all__= [__version__]
---
+++ EISy_as_Py/test/test_eisy_file_mgmt.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 12:39:28 2020
+
+@author: conta
+"""
+
+import eisy_as_py
