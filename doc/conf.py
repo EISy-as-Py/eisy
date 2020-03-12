330c721064e52eb3a3bfe8a866825a4526a5baf7
Maria Politi <politim@uw.edu>
2020-03-11 17:15:10 -0700

    more cahnges to the conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -14,8 +14,8 @@ import os
 import sys
 # import datatime
 
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../eisy'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../eisy/data/simulation'))
 
 # -- Project information -----------------------------------------------------
 
