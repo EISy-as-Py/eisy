a8e275e293234c53ddcdedc91784e86ba3acc52b
Maria Politi <politim@uw.edu>
2020-03-11 18:12:29 -0700

    more changes

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ import sys
 # import datatime
 
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../eisy'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
