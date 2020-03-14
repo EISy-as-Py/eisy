e28480abc11b8b2111e4fc2c6d126cde6362a940
Yao-Yu Li <yaoyuli@uw.edu>
2020-03-13 21:53:06 -0700

    Change the pooling factor in the calculation package.

--- EISy_as_Py/CNN_Model/PyTorch/NeuronCal.py
+++ EISy_as_Py/CNN_Model/PyTorch/NeuronCal.py
@@ -1,14 +1,13 @@
-def NC(numConv, width, height, hidden, kernel, poolSize1, poolSize2):
+def NC(numConvLayer, width, height, hidden, kernel, poolSize):
 	size = [0, 0, 0]
-	size[0] = (width - kernel + 1) / poolSize1
-	size[1] = (height - kernel + 1) / poolSize1
-	size[2] = hidden
-
-	for i in range(numLayer - 1):
-		size[0] = int((size[0] - kernel + 1) / poolSize2)
-		size[1] = int((size[1] - kernel + 1) / poolSize2)
-		size[2] = hidden * (2**(i+1))
+	for i in range(numConvLayer):
+		width = int((width - kernel + 1) / poolSize)
+		height = int((height - kernel + 1) / poolSize)
+		size[0] = width
+		size[1] = height
+		size[2] = hidden*(2**i)
+		print(size)
 
 	total = size[0]*size[1]*size[2]
-
+	print(total)
 	return total
