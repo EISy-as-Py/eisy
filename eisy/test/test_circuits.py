c0640d2c48b1c022ed30e5f35ec1032e87a4eec0
amoeezuw <amoeez@uw.edu>
2020-03-14 14:08:06 -0700

    test for RQ_series and RsRC has been added and tested.

--- eisy/test/test_circuits.py
+++ eisy/test/test_circuits.py
@@ -129,3 +129,74 @@ class TestSimulationTools(unittest.TestCase):
         for item in response:
             assert isinstance(item, complex), 'The returned response\
                 includes invalid impedance'
+
+    def test_RQ_series(self):
+        high_freq = 10**8  # Hz
+        low_freq = 0.01  # Hz
+        decades = 7
+
+        assert isinstance(decades, int),\
+            'The number of decades should be an integer'
+        assert high_freq >= low_freq,\
+            'The low frequency should be smaller than the high\
+             frequency limit value. Check again.'
+
+        f_range = circuits.freq_gen(high_freq, low_freq, decades)
+
+        Resistance = 10
+        Constant_phase_element = 10**-6
+        alpha = 1
+
+        assert np.positive(Resistance), 'The input resistance\
+            is invalid'
+        assert np.positive(Constant_phase_element), 'The input phase element\
+            is invalid'
+
+        response = circuits.cir_RQ_series(f_range[1], Resistance,
+                                          Constant_phase_element, alpha)
+
+        assert len(response) == len(f_range[1]), 'The returned response\
+            is not valid'
+
+        for item in response:
+            assert isinstance(item, complex), 'The returned response\
+                includes invalid impedance'
+
+    def test_RsRC(self):
+        high_freq = 10**8  # Hz
+        low_freq = 0.01  # Hz
+        decades = 7
+
+        assert isinstance(decades, int),\
+            'The number of decades should be an integer'
+        assert high_freq >= low_freq,\
+            'The low frequency should be smaller than the high\
+             frequency limit value. Check again.'
+
+        f_range = circuits.freq_gen(high_freq, low_freq, decades)
+
+        Solution_Resistance = 10
+        Parallel_Resistance = 100
+        Capacitance = 10**-6
+
+        assert np.positive(Solution_Resistance), 'The input resistance\
+            is invalid'
+        assert np.positive(Parallel_Resistance), 'The input resistance\
+            is invalid'
+        assert np.positive(Capacitance), 'The input capacitance\
+            is invalid'
+
+        response = circuits.cir_RQ_series(f_range[1], Solution_Resistance,
+                                          Parallel_Resistance, Capacitance)
+
+        assert len(response) == len(f_range[1]), 'The returned response\
+            is not valid'
+
+        for item in response:
+            assert isinstance(item, complex), 'The returned response\
+                includes invalid impedance'
+            real_Z = item.real
+            imag_Z = item.imag
+            total_Z = np.sqrt((real_Z**2) + (imag_Z**2))
+            assert total_Z > Solution_Resistance, 'The Impedance value\
+                returned is lower than the Solution Resistance'
