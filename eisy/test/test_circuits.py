b083900c94ee7852e233fff56b593c0f48ead780
amoeezuw <amoeez@uw.edu>
2020-03-14 13:33:21 -0700

    Added test for RC parallel circuit. Checked and working perfectly

--- eisy/test/test_circuits.py
+++ eisy/test/test_circuits.py
@@ -4,7 +4,6 @@ import csv
 import time
 import unittest
 
-
 module_path = os.path.abspath(os.path.join('../data/simulation'))
 if module_path not in sys.path:
     sys.path.append(module_path)
@@ -37,6 +36,33 @@ class TestSimulationTools(unittest.TestCase):
         assert len(f_range[0]) == len(f_range[1]), \
             'The output returned is invalid'
 
+    def test_rc_parallel(self):
+        high_freq = 10**8  # Hz
+        low_freq = 0.01  # Hz
+        decades = 7
 
-    #def test_rc_circuit(self):
+        assert isinstance(decades, int),\
+            'The number of decades should be an integer'
+        assert high_freq >= low_freq,\
+            'The low frequency should be smaller than the high\
+             frequency limit value. Check again.'
 
+        f_range = circuits.freq_gen(high_freq, low_freq, decades)
+
+        Resistance = 10
+        Capacitance = 10**-6
+
+        assert np.positive(Resistance), 'The input resistance\
+            is invalid'
+        assert np.positive(Capacitance), 'The input capacitance\
+            is invalid'
+
+        response = circuits.cir_RC_parallel(f_range[1], Resistance,
+                                            Capacitance)
+
+        assert len(response) == len(f_range[1]), 'The returned response\
+            is not valid'
+
+        for item in response:
+            assert isinstance(item, complex), 'The returned response\
+                includes invalid impedance'
