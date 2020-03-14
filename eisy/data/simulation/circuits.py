b5f05de8cd937f9d2293552d8eb8a3c66038943b
Maria Politi <politim@uw.edu>
2020-03-13 17:18:37 -0700

    fixed another data type in the circuit.py funciton. Travis CI should work now. fingers crossed

--- eisy/data/simulation/circuits.py
+++ eisy/data/simulation/circuits.py
@@ -22,7 +22,7 @@ def freq_gen(high_freq, low_freq, decades=7):
     [0] = frequency range [Hz]
     [1] = Angular frequency range [1/s]
     '''
-    f_decades = np.log10(int(high_freq)) - np.log10(int(low_freq))
+    f_decades = int(np.log10(int(high_freq)) - np.log10(int(low_freq)))
     f_range = np.logspace(np.log10(int(high_freq)), np.log10(int(low_freq)),
                           np.around(decades*f_decades), endpoint=True)
     w_range = 2 * np.pi * f_range
