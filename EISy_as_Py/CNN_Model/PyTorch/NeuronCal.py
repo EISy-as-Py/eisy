7397bd474e5921428348149a081bcd40c6646a84
Yao-Yu Li <yaoyuli@uw.edu>
2020-03-14 18:20:08 -0700

    Change the name of the function parameter.

--- EISy_as_Py/CNN_Model/PyTorch/NeuronCal.py
+++ EISy_as_Py/CNN_Model/PyTorch/NeuronCal.py
@@ -1,13 +1,12 @@
-def NC(numConvLayer, width, height, hidden, kernel, poolSize):
+def NC(numConvLayer, width, height, firstHidden, kernel, poolSize):
 	size = [0, 0, 0]
 	for i in range(numConvLayer):
 		width = int((width - kernel + 1) / poolSize)
 		height = int((height - kernel + 1) / poolSize)
 		size[0] = width
 		size[1] = height
-		size[2] = hidden*(2**i)
-		print(size)
+		size[2] = firstHidden*(2**i)
 
+	print(size)
 	total = size[0]*size[1]*size[2]
-	print(total)
 	return total
