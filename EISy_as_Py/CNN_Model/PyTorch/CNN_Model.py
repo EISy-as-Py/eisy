17868cf5bd3a275d80af2f69e1f1d1ca43d45d1a
Yao-Yu Li <yaoyuli@uw.edu>
2020-03-16 04:44:31 -0700

    Modified the data pre-processing part. In order to fit new data type.

--- EISy_as_Py/CNN_Model/PyTorch/CNN_Model.py
+++ EISy_as_Py/CNN_Model/PyTorch/CNN_Model.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from matplotlib import style
 # import skimage
-from scipy.misc import imread
+# from scipy.misc import imread
 # from IPython import display
 # from PIL import Image
 from skimage.transform import rescale
@@ -22,61 +22,52 @@ import torch.optim as optim
 
 """Data Pre-Processing"""
 
-REBUILD_DATA = True
-
-
 class EISType():
-
-    MD = "Nyquist/Missing"  # Determine the number of type and then give the directory of each type of image
-    SP = "Nyquist/SinglePeak"
-    TP = "Nyquist/TwoPeaks"
-    # TL = "Nyquist/Tail"
-    LABELS = {MD: 0, SP: 1, TP: 2}
+    
+    NS = "Nyquist/Noisy" # Determine the number of type and then give the directory of each type of image
+    SH = "Nyquist/SingleHump"
+    TH = "Nyquist/TwoHumps"
+    TL = "Nyquist/Tail"
+    LABELS = {NS:0, SH:1, TH:2, TL:3}
     training_data = []
-    mdcount = 0
-    spcount = 0
-    tpcount = 0
-    # tlcount = 0
+    nscount = 0
+    shcount = 0
+    thcount = 0
+    tlcount = 0
     
     def make_training_data(self):
-        """
-        Take the data from assigned file and transfrom images into array.
-        Also, categorize the labeled training data.
-        """
-
-        # iterate the directory
-        for label in self.LABELS:
+        for label in self.LABELS: #iterate the directory
             print(label)
-            # Iterate all the image within the directory, f -> the file name
-            for f in tqdm(os.listdir(label)):
-                # Get the full path to the image
-                path = os.path.join(label, f)
-                # Convert the image to gray scale (optional)
-                img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                img = rescale(img, 0.15)
-                self.training_data.append([np.array(img),
-                                          np.eye(3)[self.LABELS[label]]])
+            for f in tqdm(os.listdir(label)): # iterate all the image within the directory, f -> the file name               
+                path = os.path.join(label, f) # get the full path to the image
+                if "png" in path:                    
+                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # convert the iimage to gray scale (optional)
+                    img = cv2.resize(img, (800, 536))
+                    self.training_data.append([path, np.array(img), np.eye(4)[self.LABELS[label]]])                 
 
-                if label == self.MD:
-                    self.mdcount += 1
-                elif label == self.SP:
-                    self.spcount += 1
-                elif label == self.TP:
-                    self.tpcount += 1
-                # elif label == self.TL:
-                    # self.tlcount += 1
-                
+                    if label == self.NS:
+                        self.nscount += 1
+                    elif label == self.SH:
+                        self.shcount += 1
+                    elif label == self.TH:
+                        self.thcount += 1
+                    elif label == self.TL:
+                        self.tlcount += 1    
+
         np.random.shuffle(self.training_data)
         np.save("eis_training_data.npy", self.training_data)
-        print("Missing:", self.mdcount)
-        print("SinglePeak:", self.spcount)
-        print("TwoPeaks:", self.tpcount)
-        # print("Tail:", self.tlcount)
-
+        print("Noisy:", self.nscount)
+        print("SingleHump:", self.shcount)
+        print("TwoHumps:", self.thcount)
+        print("Tail:", self.tlcount)
 
 if REBUILD_DATA:
     Type = EISType()
     Type.make_training_data()
+    
+
+
+
 
 """Data Status Check"""
 
@@ -232,7 +223,9 @@ def data_separation(data, ratio_of_testing, TRAIN):
 def learning(train_data1, train_data2, input_size, image_width, image_height,
              firstHidden, kernel_size, output_size, learning_rate, BATCH_SIZE,
              EPOCHS):
-    """ """
+    """ 
+
+    """
     optimizer = optim.Adam(Net(input_size, image_width, image_height,
                                firstHidden, kernel_size, output_size
                                ).parameter(), lr=learning_rate)
@@ -258,7 +251,9 @@ def learning(train_data1, train_data2, input_size, image_width, image_height,
 
 def accuracy(test_data1, test_data2, input_size, image_width, image_height,
              firstHidden, kernel_size, output_size):
-    """ """
+    """
+
+    """
     correct = 0
     total = 0
     with torch.no_grad():
@@ -276,5 +271,6 @@ def accuracy(test_data1, test_data2, input_size, image_width, image_height,
             total += 1
 
     print("Accuracy:", round(correct/total, 3))
+
 
 
