ea287eff780f8e10c4f6961a37b5c4ec5d5201d4
Yao-Yu Li <yaoyuli@uw.edu>
2020-03-18 20:29:36 -0700

    Modified the size of testing data to avoid the stucking problem while pushing it on the github.

--- eisy/test/test_CNN_Model.py
+++ eisy/test/test_CNN_Model.py
@@ -17,14 +17,10 @@ import eisy.Neural_Network.CNN_Model as CNN
 class TestCNNModelTools(unittest.TestCase):
 
     def test_DataImporter_Training(self):
-        k = 4
-        path_List_training = ['C:/Users/joele/Desktop/Final Test/Nyquist/Noisy',
-                              'C:/Users/joele/Desktop/Final Test/Nyquist/SingleHump',
-                              'C:/Users/joele/Desktop/Final Test/Nyquist/TwoHumps',
-                              'C:/Users/joele/Desktop/Final Test/Nyquist/Tail',
-                              'training.npy']
-        image_width = 800
-        image_height = 536
+        k = 1
+        path_List_training = ['testImage', 'training.npy']
+        image_width = 200
+        image_height = 134
         train_d = CNN.EISDataImport.DataImporter_Training(self, k,
                                                           path_List_training,
                                                           image_width,
@@ -39,10 +35,9 @@ class TestCNNModelTools(unittest.TestCase):
 
     def test_DataImporter_Predict(self):
         k = 1
-        path_List_predict = ['C:/Users/joele/Desktop/Final Test/Nyquist/Random',
-                             'predicting.npy']
-        image_width = 800
-        image_height = 536
+        path_List_predict = ['testImage', 'training.npy']
+        image_width = 200
+        image_height = 134
 
         predict_d = CNN.EISDataImport.DataImporter_Predict(self, k,
                                                            path_List_predict,
@@ -59,14 +54,10 @@ class TestCNNModelTools(unittest.TestCase):
     def test_Build_Data(self):
         Training = True
         Predict = False
-        k = 4
-        path_list = ['C:/Users/joele/Desktop/Final Test/Neural_Network/Nyquist/Noisy',
-                     'C:/Users/joele/Desktop/Final Test/Neural_Network/Nyquist/SingleHump',
-                     'C:/Users/joele/Desktop/Final Test/Neural_Network/Nyquist/TwoHumps',
-                     'C:/Users/joele/Desktop/Final Test/Neural_Network/Nyquist/Tail',
-                     'training.npy']
-        image_width = 800
-        image_height = 536
+        k = 1
+        path_list = ['testImage', 'training.npy']
+        image_width = 200
+        image_height = 134
 
         Build_d = CNN.Build_Data(Training, Predict, k, path_list,
                                  image_width, image_height)
@@ -81,7 +72,7 @@ class TestCNNModelTools(unittest.TestCase):
         assert image_height <= 1000, 'Image size is too large.'
 
     def test_load_array_data(self):
-        np_ndarray_file = 'predicting.npy'
+        np_ndarray_file = 'training.npy'
 
         load_array_d = CNN.load_array_data(np_ndarray_file)
         assert type(np_ndarray_file) == str, \
@@ -89,11 +80,11 @@ class TestCNNModelTools(unittest.TestCase):
 
     def test_data_information(self):
         input_data = []
-        IMG = np.random.rand(536, 800)
-        for i in range(140):
+        IMG = np.random.rand(134, 200)
+        for i in range(1):
             input_data.append(['path', np.array(IMG), np.eye(4)[1]])
-        np.save('test.npy', input_data)
-        array_data = np.load('test.npy', allow_pickle=True)
+        np.save('training.npy', input_data)
+        array_data = np.load('training.npy', allow_pickle=True)
 
         data_information = CNN.data_information(array_data)
         assert type(array_data) == np.ndarray, \
@@ -101,8 +92,8 @@ class TestCNNModelTools(unittest.TestCase):
 
     def test_ploting_data(self):
         input_data = []
-        IMG = np.random.rand(536, 800)
-        for j in range(140):
+        IMG = np.random.rand(134, 200)
+        for j in range(5):
             input_data.append(['path', np.array(IMG), np.eye(4)[1]])
         i = 1
         ploting_d = CNN.ploting_data(input_data, i)
@@ -112,13 +103,13 @@ class TestCNNModelTools(unittest.TestCase):
 
     def test_image_to_tensor(self):
         input_data = []
-        IMG = np.random.rand(536, 800)
-        for i in range(140):
+        IMG = np.random.rand(134, 200)
+        for i in range(1):
             input_data.append(['path', np.array(IMG), np.eye(4)[1]])
-        np.save('test.npy', input_data)
-        array_data = np.load('test.npy', allow_pickle=True)
-        image_width = 800
-        image_height = 536
+        np.save('training.npy', input_data)
+        array_data = np.load('training.npy', allow_pickle=True)
+        image_width = 200
+        image_height = 134
 
         image_to_tensor = CNN.image_to_tensor(array_data, image_width,
                                               image_height)
@@ -127,11 +118,11 @@ class TestCNNModelTools(unittest.TestCase):
 
     def test_type_to_tensor(self):
         input_data = []
-        IMG = np.random.rand(536, 800)
-        for i in range(140):
+        IMG = np.random.rand(134, 200)
+        for i in range(1):
             input_data.append(['path', np.array(IMG), np.eye(4)[1]])
-        np.save('test.npy', input_data)
-        array_data = np.load('test.npy', allow_pickle=True)
+        np.save('training.npy', input_data)
+        array_data = np.load('training.npy', allow_pickle=True)
 
         type_to_tensor = CNN.type_to_tensor(array_data)
         assert type(array_data) == np.ndarray, \
@@ -141,8 +132,8 @@ class TestCNNModelTools(unittest.TestCase):
         ratio_of_test = 0.2
         TRAIN = True
         TEST = False
-        image_width = 800
-        image_height = 536
+        image_width = 200
+        image_height = 134
         tensor_data = torch.randn(image_height, image_width).view(-1, 1, image_height, image_width)
 
         d_separation = CNN.data_separation(tensor_data, ratio_of_test,
@@ -155,8 +146,8 @@ class TestCNNModelTools(unittest.TestCase):
         assert TRAIN != TEST, 'Return only one type of sample in one time.'
 
     def test_learning(self):
-        image_width = 800
-        image_height = 536
+        image_width = 200
+        image_height = 134
         training_sample_image = torch.randn(image_height, image_width).view(-1, 1, image_height, image_width)
         training_sample_type = torch.randn(1, 4)
         input_size = 1
@@ -177,8 +168,8 @@ class TestCNNModelTools(unittest.TestCase):
         assert kernel_size % 2 == 1, 'kernel_size should be an odd integer'
 
     def test_accuracy(self):
-        image_width = 800
-        image_height = 536
+        image_width = 200
+        image_height = 134
         testing_sample_image = torch.randn(image_height, image_width).view(-1, 1, image_height, image_width)
         testing_sample_type = torch.randn(1, 4)
         input_size = 1
@@ -197,16 +188,16 @@ class TestCNNModelTools(unittest.TestCase):
     def test_type_prediction(self):
         k = 1
         input_size = 1
-        image_width = 800
-        image_height = 536
-        path_List_training = ['C:/Users/joele/Desktop/Final Test/Neural_Network/Nyquist/Random', 'predicting.npy']
+        image_width = 200
+        image_height = 134
+        path_List_training = ['testImage', 'training.npy']
         tensor_data = torch.randn(image_height, image_width).view(-1, 1, image_height, image_width)
         input_data = []
-        IMG = np.random.rand(536, 800)
-        for i in range(140):
+        IMG = np.random.rand(134, 200)
+        for i in range(1):
             input_data.append(['path', np.array(IMG), np.eye(4)[1]])
-        np.save('test.npy', input_data)
-        array_data = np.load('test.npy', allow_pickle=True)
+        np.save('training.npy', input_data)
+        array_data = np.load('training.npy', allow_pickle=True)
         firstHidden = 8
         kernel_size = 5
         output_size = 4
